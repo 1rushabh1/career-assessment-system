6651589f462ce9a294925dba01b8e6246da3f2ce
1rushabh1 <rushabhthaware1>
2026-03-06 10:17:45 +0530

    fix api route

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const resultRoute = require("./routes/result");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://career-assessment-system.onrender.com/"
+  baseURL: "https://career-assessment-system.onrender.com/api"
 });
 
 // Automatically attach token
